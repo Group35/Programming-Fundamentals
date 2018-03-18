0b3a3933c3d4e74bbb9817c6cb4e66f43898a6e6
LazarPanchev <l.panchev@abv.bg>
2018-03-18 14:44:44 +0200

    Document the code of problems 4 and 5

--- Problem4/Program.cs
+++ Problem4/Program.cs
@@ -6,12 +6,16 @@ namespace BeverageLabels
     {
         static void Main(string[] args)
         {
+			//We receive from the console product name in the first line- a string
             string name = Console.ReadLine();
+			/*In the next three lines, we receive volume, energy and sugar content per 100ml.
+			as integers*/
             int volume = int.Parse(Console.ReadLine());
             int energyPer100 = int.Parse(Console.ReadLine());
             int sugarPer100 = int.Parse(Console.ReadLine());
+			//We calculate the current volume
             double volume2 = volume / 100.0;
-
+            //Finally we calculate the energy and sugar content for current volume. Print them on the console
             Console.WriteLine($"{volume}ml {name}:");
             Console.WriteLine($"{energyPer100 * volume2}kcal, {sugarPer100*volume2}g sugars");
         }
--- Problem5/Program.cs
+++ Problem5/Program.cs
@@ -6,11 +6,12 @@ namespace CharacterStats
     {
         static void Main(string[] args)
         {
+			/*We receive from the console four lines with card numbers*/
             int first = int.Parse(Console.ReadLine());
             int second = int.Parse(Console.ReadLine());
             int third = int.Parse(Console.ReadLine());
             int fourth = int.Parse(Console.ReadLine());
-
+            // We print them on the console using formatting for the zeroes 
             Console.WriteLine($"{first:d4} {second:d4} {third:d4} {fourth:d4}");
 
             
