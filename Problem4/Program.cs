4358607c89c09d093228a8e1fce052ee9e3aa83f
AnaVelcheva <AnaVelcheva@students.softuni.bg>
2018-03-18 10:32:14 +0200

    Ana added solution for problem 4

---
+++ Problem4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BeverageLabels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energyPer100 = int.Parse(Console.ReadLine());
+            int sugarPer100 = int.Parse(Console.ReadLine());
+            double volume2 = volume / 100.0;
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{energyPer100 * volume2}kcal, {sugarPer100*volume2}g sugars");
+        }
+    }
+}
